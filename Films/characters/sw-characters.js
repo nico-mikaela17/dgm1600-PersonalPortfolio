8070fc8665847a19178b30e4556b129e626e3414
Nicole Linnarz <mikaelafs17@gmail.com>
2022-03-17 15:00:33 -0600

    added more characters

--- Films/characters/sw-characters.js
+++ Films/characters/sw-characters.js
@@ -1,24 +1,43 @@
-import {people} from '../data/people.js'
+import { people } from '../data/people.js'
 
 const header = document.querySelector('header')
 const main = document.querySelector('main')
 
-const allCharactersButton = document.createElement('button')
-allCharactersButton.textContent = 'All Characters'
-allCharactersButton.addEventListener('click',function (event) {
-    console.log ('Thanks for clicking!')
+const allCharsButton = document.createElement('button')
+allCharsButton.textContent = 'All Characters'
+allCharsButton.addEventListener('click', function () {
+  console.log('Thanks for clicking')
+  populateDOM(people)
 })
+header.appendChild(allCharsButton)
 
-header.appendChild(allCharactersButton)
+const maleCharacters = people.filter(person => person.gender === 'male')
 
-people.forEach((person) => { 
-const personFig = document.createElement('figure')
-const personTag = document.createElement('img')
-personImg.src = 'https://starwars-visualguide.com/assets/img/characters/1.jpg'
-const personCap = document.createElement('figcaption')
+const maleCharsButton = document.createElement('button')
+maleCharsButton.textContent = 'Male Characters'
+maleCharsButton.addEventListener('click', () => console.log("Thanks for clicking on Male Character button"))
 
+function populateDOM(characters) {
+  people.forEach((person) => {
+    const personFig = document.createElement('figure')
+    const personImg = document.createElement('img')
+    let charNum = getLastNumber(person.url)
+    personImg.src = `https://starwars-visualguide.com/assets/img/characters/${charNum}.jpg`
+    const personCap = document.createElement('figcaption')
+    personCap.textContent = person.name
 
-personFig.appendChild(personTag)
-personFig.appendChild(personCap)
-main.appendChild(personFig)
-)}
+    personFig.appendChild(personImg)
+    personFig.appendChild(personCap)
+    main.appendChild(personFig)
+  })
+}
+
+function getLastNumber(url) {
+  let end = url.lastIndexOf('/')
+  let start = end -2
+  if (url.charAt(start) === '/') {
+    start++
+  }
+  return url.slice(start,end)
+}
+  
--- Films/films/film-script.js
+++ Films/films/film-script.js
@@ -1,18 +1,33 @@
-import {films} from "../data/films.js"
+import { films } from '../data/films.js'
 
 console.log(films[0].url)
 
 function getLastNumber(url) {
-    const secondToLastLetterOfUrl = url[url.length - 2]
-    return secondToLastLetterOfUrl//return the second to last number from the url property of a film object
+  const secondToLastLetterOfUrl = url[url.length - 2]
+  return secondToLastLetterOfUrl// return the second to last number from the url property of a film object
 }
 
-console.log(getLastNumber['https://swapi.co/api/films/7/'])
+  // First, get a reference to the main elememt with the id 'filmList' and store it in a variable
+  let filmList = document.querySelector('#filmList')
 
-let filmList = document.querySelector('#filmList')
+// for (initialize variable; condition check; increment variable)
+for (let i = 0; i < films.length; i++) {
 
-let newImage = document.createElement('img')
+// Second, use the document.createElement function/method to create a new img element
 
-newImage.src = 'https://starwars-visualguide.com/assets/img/films/6.jpg'
+let figure = document.createElement('figure')
+let figImage = document.createElement('img')
+let figCaption = document.createElement('figcaption')
 
-filmList.appendChild(newImage)
+// Third, set the source property of the new image to a valid URL or path
+
+let filmNum = getLastNumber(films[i].url)
+figImage.src = `https://starwars-visualguide.com/assets/img/films/${filmNum}.jpg`
+figCaption.textContent = films[i].title
+
+// Fourth, add the newly created image as a child of the main element
+figure.appendChild(figImage)
+figure.appendChild(figCaption)
+
+filmList.appendChild(figure)
+}
