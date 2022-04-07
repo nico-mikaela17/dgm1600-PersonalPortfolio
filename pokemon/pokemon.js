ed15332b01c729426414edbf2f75aa7c7ad9b0b4
Nicole Linnarz <mikaelafs17@gmail.com>
2022-04-07 11:41:40 -0600

    added pokemon info

---
+++ pokemon/pokemon.js
@@ -0,0 +1,11 @@
+const getAPIData = async (url) => {
+    try {
+      const result = await fetch(url)
+      const data = await result.json()
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  
+  getAPIData('https://pokeapi.co/api/v2/pokemon/snorlax')
